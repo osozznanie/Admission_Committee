8c7a4ec54dd462f99ab035150af16d2f77952ff8
Vlad <vlad.chertorygin18@gmail.com>
2023-11-03 13:59:00 +0200

    added dto

---
+++ src/main/java/com/example/admissionCommittee/dto/SubjectDTO.java
@@ -0,0 +1,18 @@
+import com.example.admissionCommittee.dto.AbstractDTO;
+
+public class SubjectDTO extends AbstractDTO implements Comparable<SubjectDTO>{
+
+    public SubjectDTO(Integer id, String title) {
+        super(id, title);
+    }
+
+    @Override
+    public int compareTo(SubjectDTO subjectDTO) {
+        if (this.id > subjectDTO.id) {
+            return 1;
+        } else if (this.id < subjectDTO.id) {
+            return -1;
+        }
+        return 0;
+    }
+}
